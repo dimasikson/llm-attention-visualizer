a4dcf277ab3760018888c7fef24b73c810b72504
Dmitri Ivanov <dmitriivanov@microsoft.com>
2023-07-15 23:59:09 +0300

    Requirements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
 import requests
 import json
+
+from flask import Flask, render_template, request
 
 from azureml.core import Workspace
 from azureml.core.authentication import ServicePrincipalAuthentication
